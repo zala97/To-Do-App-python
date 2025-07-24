2005258f28f591917a19e4a6d2c6a638b23dfb9b
Ali-Raven <alimateeee@gmail.com>
2025-07-24 15:09:51 +0330

    add functions for implementing the feature , develope the Add_Task function and struct the data that gives from the FronEnd (or GUI side)

--- Features/feature.py
+++ Features/feature.py
@@ -1,1 +1,29 @@
-print ("hello zali")
+import time
+from datetime import datetime
+# Docstring by Alimate
+def Add_Task():
+    '''
+        Create the sturcture of the task and pass it to the server side
+    '''
+    # get the currnet date and time
+    current_date_time = datetime.today()
+
+    data_structure = {
+        "Title" : '',
+        "Desc" : '',
+        "Priority" : '',
+        "Date" : '',
+        "Create at" : f'{current_date_time}'
+    }
+
+def Remove_Task():
+    pass
+
+def Edit_Task():
+    pass
+
+def Reminders():
+    pass
+
+def Get_list_of_Tasks():
+    pass
